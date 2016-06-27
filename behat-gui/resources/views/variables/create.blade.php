b73f1dd17e01f13d7d6cd2f644a4deade9dc6f0f
Ryan Gurnick <ryangurnick@gmail.com>
2016-06-27 15:02:42 -0700

    Finished Variable Creator

--- behat-gui/resources/views/variables/create.blade.php
+++ behat-gui/resources/views/variables/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 @section('css')
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/css/bootstrap-datepicker.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/css/bootstrap-datepicker.css" rel="stylesheet">
 @endsection
 @section('header')
     <div class="page-header">
@@ -16,19 +16,19 @@
             <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
             <div class="form-group @if($errors->has('key')) has-error @endif">
-                   <label for="key-field">Key</label>
-                <input type="text" id="key-field" name="key" class="form-control" value="{{ old("key") }}"/>
-                   @if($errors->has("key"))
+                <label for="key-field">Key</label>
+                <input type="text" id="default-key" name="key" class="form-control" value="{{ old("key") }}"/>
+                @if($errors->has("key"))
                     <span class="help-block">{{ $errors->first("key") }}</span>
-                   @endif
-                </div>
-                <div class="form-group @if($errors->has('value')) has-error @endif">
-                   <label for="value-field">Default Value</label>
-                <input type="text" id="value-field" name="default-value" class="form-control" value="{{ old("value") }}"/>
-                   @if($errors->has("value"))
+                @endif
+            </div>
+            <div class="form-group @if($errors->has('value')) has-error @endif">
+                <label for="value-field">Default Value</label>
+                <input type="text" id="default-value" name="default-value" class="form-control" value="{{ old("value") }}"/>
+                @if($errors->has("value"))
                     <span class="help-block">{{ $errors->first("value") }}</span>
-                   @endif
-                </div>
+                @endif
+            </div>
             <hr />
             <a id="add_set" class="btn btn-primary">Add Variable Set</a><br/><br />
             <div id="set_area"></div>
@@ -40,41 +40,55 @@
     </div>
 @endsection
 @section('scripts')
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/js/bootstrap-datepicker.min.js"></script>
-  <script>
-    $('.date-picker').datepicker({
-    });
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/js/bootstrap-datepicker.min.js"></script>
+    <script>
+        $('.date-picker').datepicker({
+        });
 
 
-    var num = 0;
-    $("#add_set").on("click", function(){
-        $("#set_area").append("<div class='form-group'><label for='value-field-"+num+"'>Value "+num+"</label><input type='text' id='value-field-"+num+"' name='value_"+num+"' class='form-control' value=''/></div> <div class='form-group'> <label for='set-field-"+num+"'>Set "+num+"</label><select id='set-field-"+num+"' name='set_"+num+"' id class='form-control'>@foreach(\App\Set::all() as $s) <option value='{{ $s->id  }}'>{{ $s->name  }}</option> @endforeach</select></div>");
-        num += 1;
-    });
+        var num = 0;
+        $("#add_set").on("click", function(){
+            $("#set_area").append("<div class='form-group'><label for='value-field-"+num+"'>Value "+num+"</label><input type='text' id='value-field-"+num+"' name='value_"+num+"' class='form-control' value=''/></div> <div class='form-group'> <label for='set-field-"+num+"'>Set "+num+"</label><select id='set-field-"+num+"' name='set_"+num+"' id class='form-control'>@foreach(\App\Set::all() as $s) <option value='{{ $s->id  }}'>{{ $s->name  }}</option> @endforeach</select></div>");
+            num += 1;
+        });
 
-    $("#set_create").on("click", function(){
-        var final = {};
-        var set_ids = $('select').filter(function() { return this.id.match(/set-field-([0-9]+)/); });
-        $(set_ids).each(function(n, i){
-            var id = "#"+i.id;
-            var num = id.split('-');
+        $("#set_create").on("click", function(){
+            var final = {};
+            var main = {};
+            var set_ids = $('select').filter(function() { return this.id.match(/set-field-([0-9]+)/); });
+            $(set_ids).each(function(n, i){
+                var id = "#"+i.id;
+                var num = id.split('-');
 
-            final[num[num.length-1]] = {};
-            final[num[num.length-1]].select = $(id).val();
+                final[num[num.length-1]] = {};
+                final[num[num.length-1]].select = $(id).val();
+
+            });
+
+            var input_ids = $('input').filter(function() { return this.id.match(/value-field-([0-9]+)/); });
+            $(input_ids).each(function(n, i){
+                var id = "#"+i.id;
+                var num = id.split('-');
+
+                final[num[num.length-1]].input = $(id).val();
+
+            });
+            main['key'] = $("#default-key").val();
+            main['value'] = $("#default-value").val();
+
+            $.ajaxSetup({ headers: { 'X-CSRF-TOKEN' : '{{ csrf_token() }}' } });
+
+
+            $.ajax({
+                url: '{{ route('variables.store') }}',
+                method: 'POST',
+                data: "data="+JSON.stringify(final)+"&main="+JSON.stringify(main),
+                complete: function(r){
+                    $("body").html(r.responseText);
+                }
+            });
 
         });
 
-        var input_ids = $('input').filter(function() { return this.id.match(/value-field-([0-9]+)/); });
-        $(input_ids).each(function(n, i){
-            var id = "#"+i.id;
-            var num = id.split('-');
-
-            final[num[num.length-1]].input = $(id).val();
-
-        });
-        jsonString = JSON.stringify(final);
-        
-    });
-
-  </script>
+    </script>
 @endsection
