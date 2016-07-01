89f42632814ff1a3041c61d8c3155a016853e4a9
Ryan Gurnick <ryangurnick@gmail.com>
2016-07-01 09:28:32 -0700

    Fixed Class Redeclare

--- behat-gui/database/migrations/2016_07_01_162112_create_tests_results_table.php
+++ behat-gui/database/migrations/2016_07_01_162112_create_tests_results_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTestsTable extends Migration {
+class CreateTestsResultsTable extends Migration {
 
     /**
      * Run the migrations.
