b906178cfa4d5cd546c431663eb7536e3dc9e46f
Ryan Gurnick <ryangurnick@gmail.com>
2016-07-01 09:22:37 -0700

    Added Migration

---
+++ behat-gui/database/migrations/2016_07_01_162112_create_tests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTestsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tests_results', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('test_id');
+            $table->text('result');
+            $table->integer('success');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tests_results');
+    }
+
+}
