b8f53cbee3ad0398301ed5979c1f74c51ecf0d1d
Ryan Gurnick <ryangurnick@gmail.com>
2016-07-05 16:54:17 -0700

    Added JGrowl

--- behat-gui/resources/views/layout.blade.php
+++ behat-gui/resources/views/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.1/jquery.jgrowl.min.css" />
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
@@ -68,6 +69,21 @@
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <!-- <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script> -->
     <script type="text/javascript" src="{{ asset('jjsonviewer.js')  }}"></script>
+
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.1/jquery.jgrowl.min.js"></script>
+
+    <script type="application/javascript">
+
+    @if ($errors->has())
+        @foreach ($errors->all() as $error)
+            $.jGrowl("{{ $error  }}", { header: 'Error', position: 'bottom-right', life: 10000 });
+        @endforeach
+    @endif
+    @if (session('message'))
+        $.jGrowl("{{ session('message')  }}", { header: 'Success', position: 'bottom-right', life: 10000 });
+        @endif
+    </script>
+
     @yield('scripts')
 
     <script type="text/javascript">
