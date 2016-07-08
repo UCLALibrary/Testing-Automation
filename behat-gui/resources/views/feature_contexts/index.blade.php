12b3678b572676467a5577d1b2bdcd8b9c7be9ca
Ryan Gurnick <ryangurnick@gmail.com>
2016-07-08 15:19:38 -0700

    Removed Unused Button

--- behat-gui/resources/views/feature_contexts/index.blade.php
+++ behat-gui/resources/views/feature_contexts/index.blade.php
@@ -4,7 +4,6 @@
     <div class="page-header clearfix">
         <h1>
             <i class="glyphicon glyphicon-align-justify"></i> FeatureContexts
-            <a class="btn btn-success pull-right" href="{{ route('feature_contexts.create') }}"><i class="glyphicon glyphicon-plus"></i> Create</a>
         </h1>
 
     </div>
