48cc6ebc14fce2e8ec5cb82fb224ce0de9cee05a
Ryan Gurnick <ryangurnick@gmail.com>
2016-07-08 13:02:53 -0700

    Made Comment a list

--- behat-gui/resources/views/tests/show.blade.php
+++ behat-gui/resources/views/tests/show.blade.php
@@ -36,12 +36,16 @@
             <div class="row">
             <div class="col-sm-8">{!! str_replace("class=\"list-group\"","class=\"list-group\" style=\"overflow-y:scroll;\"", str_replace('data-toggle="collapse" data-parent="#accordion" href="#scenario-1-1" aria-expanded="true" aria-controls="scenario-1-1"', "", str_replace("col-sm-8", "col-sm-12", $r->result))) !!}</div>
             <div class="col-sm-4">
+                @if($r->comment_complete == 1)
                 <div class="panel panel-default">
                     <div class="panel-heading">Analysis of Message</div>
                     <div class="panel-body">
-                        {{ $r->comment  }}
+                        <ul>
+                        @foreach(explode("\n", $r->comment) as $e)<li>{!!  $e  !!}</li>@endforeach
+                        </ul>
                     </div>
                 </div>
+                @endif
                 <div class="panel panel-default">
                     <div class="panel-heading">More Info</div>
                     <div class="panel-body">
@@ -51,6 +55,7 @@
                 </div>
             </div>
             </div>
+            <hr />
         @endforeach
     </div>
     <div class="row">
