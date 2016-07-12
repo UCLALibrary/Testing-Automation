394217bcb1189b82460ae876b528ebbbc60c3240
Ryan Gurnick <ryangurnick@gmail.com>
2016-07-12 13:44:26 -0700

    Fixed Mysql Overflow

--- behat-gui/app/Console/Commands/ExecuteFeature.php
+++ behat-gui/app/Console/Commands/ExecuteFeature.php
@@ -70,12 +70,31 @@ class ExecuteFeature extends Command
 
             $result = new TestResult();
             $result->test_id = $t->id;
-            $result->result = $r;
+            $result->result = $this->sanitize_output($r);
             $result->success = $s;
             $result->save();
         }
 
         rrmdir('features/report');
         unlink($name.".feature");
+    }
+
+    function sanitize_output($buffer) {
+
+        $search = array(
+            '/\>[^\S ]+/s',  // strip whitespaces after tags, except space
+            '/[^\S ]+\</s',  // strip whitespaces before tags, except space
+            '/(\s)+/s'       // shorten multiple whitespace sequences
+        );
+
+        $replace = array(
+            '>',
+            '<',
+            '\\1'
+        );
+
+        $buffer = preg_replace($search, $replace, $buffer);
+
+        return $buffer;
     }
 }
