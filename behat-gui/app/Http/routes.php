70134c433373433fa6fa6ec7e0925b5a3b42d3c7
Lilly An <li.liwei94@gmail.com>
2016-11-01 16:25:35 -0700

    payload

--- behat-gui/app/Http/routes.php
+++ behat-gui/app/Http/routes.php
@@ -9,7 +9,7 @@ Route::get('auth/github/callback', 'Auth\AuthController@handleProviderCallback')
 /**
  * Route for github payloads
  */
-Route::post('/payload', ['as' => 'triggers.githubpayload', 'uses' => 'TriggerController@github']);
+Route::post('/', ['as' => 'triggers.githubpayload', 'uses' => 'TriggerController@github']);
 /**
  * Route group to require authentication in all of the system routes
  */
