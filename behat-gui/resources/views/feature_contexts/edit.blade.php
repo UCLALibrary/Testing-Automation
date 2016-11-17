0489f9efb41138c12a4b13ff0820a5873a9edf4b
Lilly An <li.liwei94@gmail.com>
2016-11-17 14:57:06 -0800

    updated feature context edit

--- behat-gui/resources/views/feature_contexts/edit.blade.php
+++ behat-gui/resources/views/feature_contexts/edit.blade.php
@@ -3,27 +3,27 @@
   <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/css/bootstrap-datepicker.css" rel="stylesheet">
 @endsection
 @section('header')
-    <div class="page-header">
-        <h1><i class="glyphicon glyphicon-edit"></i> Edit Feature Contexts</h1>
-    </div>
+
+        <h1><i class="edit icon"></i> Edit Feature Contexts</h1>
+
 @endsection
 
 @section('content')
     @include('error')
 
-    <div class="row">
-        <div class="col-md-12">
+    <div class="ui piled segment">
+        <div class="ui form">
             <form action="{{ route('feature_contexts.update', 1)  }}" method="POST">
                 <input type="hidden" name="_method" value="PUT">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-                <div class="form-group">
+                <div class="field">
                     <textarea name="feature_context" class="form-control">{{ $feature_context  }}</textarea>
                 </div>
 
                 <div class="well well-sm">
-                    <button type="submit" class="btn btn-primary">Save</button>
-                    <a class="btn btn-link pull-right" href="{{ route('feature_contexts.index') }}"><i class="glyphicon glyphicon-backward"></i>  Back</a>
+                    <button type="submit" class="ui green button">Save</button>
+                    <a class="ui grey button" href="{{ route('feature_contexts.index') }}"><i class="reply icon"></i>  Back</a>
                 </div>
             </form>
         </div>
